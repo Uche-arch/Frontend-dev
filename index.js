8504237f57802632a72bc840a6f34f97c0a138ed
Uche Godswill <auchjee@gmail.com>
2025-03-04 21:57:05 +0100

    Initial commit on this project

---
+++ index.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    document.body.classList.add("loaded");
+  }, 2000); // Adjust the timeout for how long you want the loading screen to stay
+});
+
+
